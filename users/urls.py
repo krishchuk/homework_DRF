af7a314e0413a9d2abd577da0943f3b5929fdb61
krishchuk <ms.den.best@mail.ru>
2024-05-14 22:17:28 +0500

    JWT auth fix permission_classes in urls

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import SimpleRouter
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView,)
 
@@ -10,7 +11,7 @@ app_name = UsersConfig.name
 router = SimpleRouter()
 router.register('', UserViewSet, basename='user')
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('login/', TokenObtainPairView.as_view(permission_classes=[AllowAny]), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     path('pay/', PayListAPIView.as_view(), name='pay_list'),
