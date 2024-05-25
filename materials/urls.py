5dd07c5b0d37f397fc192616a88aaf0404685868
krishchuk <ms.den.best@mail.ru>
2024-05-25 19:28:21 +0500

    fix lesson urls and add subscription urls

--- materials/urls.py
+++ materials/urls.py
@@ -3,7 +3,8 @@ from rest_framework.routers import SimpleRouter
 
 from materials.apps import MaterialsConfig
 from materials.views import (CourseViewSet, LessonCreateAPIView, LessonListAPIView,
-                             LessonRetrieveAPIView, LessonUpdateAPIView, LessonDestroyAPIView)
+                             LessonRetrieveAPIView, LessonUpdateAPIView, LessonDestroyAPIView,
+                             SubscriptionCreateAPIView, SubscriptionListAPIView)
 
 app_name = MaterialsConfig.name
 
@@ -12,7 +13,10 @@ router.register('', CourseViewSet, basename='course')
 urlpatterns = [
     path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
     path('lesson/', LessonListAPIView.as_view(), name='lesson_list'),
-    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_create'),
-    path('lesson/<int:pk>/update/', LessonUpdateAPIView.as_view(), name='lesson_create'),
-    path('lesson/<int:pk>/delete/', LessonDestroyAPIView.as_view(), name='lesson_create'),
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_get'),
+    path('lesson/<int:pk>/update/', LessonUpdateAPIView.as_view(), name='lesson_update'),
+    path('lesson/<int:pk>/delete/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
+
+    path('subscription/create/', SubscriptionCreateAPIView.as_view(), name='subscription_create'),
+    path('subscription/', SubscriptionListAPIView.as_view(), name='subscription_list'),
 ] + router.urls
