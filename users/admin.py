d94f75b7ff987983855837ae1a60ae068d702553
krishchuk <ms.den.best@mail.ru>
2024-05-27 20:03:46 +0500

    Расширил инфу в админке для User и Pay

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,29 @@ from django.contrib import admin
 
 from users.models import User, Pay
 
-admin.site.register(User)
 
-admin.site.register(Pay)
+@admin.register(User)
+class AdminUser(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "email",
+        "phone",
+        "city",
+    )
+
+
+@admin.register(Pay)
+class AdminPay(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "pay_date",
+        "paid_course",
+        "paid_lesson",
+        "payment_amount",
+        "payment_method",
+        "session_id",
+        "link",
+    )
