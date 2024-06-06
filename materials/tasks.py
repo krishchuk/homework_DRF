067d8a88e19a877cbde34fc21a9eb6990f726da5
krishchuk <ms.den.best@mail.ru>
2024-06-06 19:09:25 +0500

    Оптимизировал рассылку сообщений об обновлении

--- materials/tasks.py
+++ materials/tasks.py
@@ -7,11 +7,12 @@ from materials.models import Subscription
 
 @shared_task
 def send_mail_update(course_id):
-    subscriptions = [sub.user.email for sub in Subscription.objects.filter(course_id=course_id)]
+    subscriptions = Subscription.objects.filter(course_id=course_id)
+    recipients = subscriptions.select_related('user').values_list('user__email', flat=True)
     send_mail(
         subject="Обновление курса на платформе!",
         message=f"Курс {subscriptions.course.title} был обновлен!",
         from_email=settings.EMAIL_HOST_USER,
-        recipient_list=subscriptions,
+        recipient_list=recipients,
         fail_silently=False,
     )
